d7290b263a65aec929afaf6f4099fd1f72ebd966
Amanda <amanda.r.merrifield@gmail.com>
2020-06-29 01:24:26 +0100

    models and admin

--- src/marketplace/admin.py
+++ src/marketplace/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from marketplace.models import Image, Post, Location, LocationTag
+
+class LocationInline(admin.TabularInline):
+  model = LocationTag
+  extra = 1
+
+class ImageAdmin(admin.ModelAdmin):
+  list_display = ['photographer', 'date_taken']
+admin.site.register(Image, ImageAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ['price', 'image']
+admin.site.register(Post, PostAdmin)
+
+class LocationAdmin(admin.ModelAdmin):
+  list_display = ['name']
+  inlines = (LocationInline,)
+admin.site.register(Location, LocationAdmin)
+
+
+class LocationTagAdmin(admin.ModelAdmin):
+  list_display = ['image','location']
+admin.site.register(LocationTag, LocationTagAdmin)
--- src/marketplace/models.py
+++ src/marketplace/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Image(models.Model):
+    file = models.ImageField()
+    date_taken = models.DateTimeField()
+    description = models.TextField()
+    photographer = models.ForeignKey(User, on_delete=models.PROTECT)
+    def __str__(self): return self.file.name + self.photographer.first_name + self.photographer.last_name
+
+class Post(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT)
+
+
+
+
+class Location(models.Model):
+    name = models.TextField()
+    images = models.ManyToManyField(Image, related_name='locations', through='LocationTag')
+
+
+class LocationTag(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
